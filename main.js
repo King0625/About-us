12a3ae22e58f78812ee6c11daae6566d51900f01
King0625 <a7164265@gmail.com>
2019-04-15 18:48:53 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    $("#img1").hover(
+        function(){            
+            $("#person1").show();
+            $("#intro").hide();           
+        },
+        function() {        
+            $("#intro").show();
+            $("#person1").hide();            
+        }
+    );
+    $("#img2").hover(
+        function(){            
+            $("#person2").show();
+            $("#intro").hide();           
+        },
+        function() {        
+            $("#intro").show();
+            $("#person2").hide();            
+        }
+    );
+    $("#img3").hover(
+        function(){            
+            $("#person3").show();
+            $("#intro").hide();           
+        },
+        function() {        
+            $("#intro").show();
+            $("#person3").hide();            
+        }
+    );
+    $("#img4").hover(
+        function(){            
+            $("#person4").show();
+            $("#intro").hide();           
+        },
+        function() {        
+            $("#intro").show();
+            $("#person4").hide();            
+        }
+    );
+    $("#img5").hover(
+        function(){            
+            $("#person5").show();
+            $("#intro").hide();           
+        },
+        function() {        
+            $("#intro").show();
+            $("#person5").hide();            
+        }
+    );
+    
+});
